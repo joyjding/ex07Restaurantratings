4970512fd2c6c29c63c57f5a81523569d92ca90e
Cara <cara.marie@me.com>
2013-06-24 18:19:02 -0700

    Ex7 with interactive mode and an easter egg.

--- sorted_data.py
+++ sorted_data.py
@@ -1,20 +1,11 @@
-from sys import argv
+# from sys import argv
 
-script, filename = argv
+# script, filename = argv
 
-f = open(filename)
+# f = open(filename)
 
-# for line in f:
-# 	split = f.readline()
-# f_read = f.read()
-# f.close()
+f = open("scores.txt")
 
-# strip = f_read.strip("\n")
-# print strip
-
-# split = strip.
-# while linenumber <= len(f):
-# 	print f.readline()
 restaurant_ratings = {}
 
 for line in f:
@@ -23,7 +14,25 @@ for line in f:
 	num = int(split[1])
 	restaurant_ratings[split[0]] = num
 
-for restaurant, rating in restaurant_ratings.iteritems():
-	print "Restaurant %s is rated at %d." % (restaurant, rating)
+# for restaurant, rating in restaurant_ratings.iteritems():
+# 	print "Restaurant %s is rated at %d." % (restaurant, rating)
 
+def rest_rate():
+	restaurant = raw_input("\vWhich restaurant would you like to know about?\n> ")
+	print "The rating for %s is %d" % (restaurant, restaurant_ratings[restaurant])
 
+	while True:
+		print "\vWhat other restaurant are you interested in?"
+		next = raw_input("> ")
+		
+		if next == "hungry":
+			print "om nom nom :D"
+			break
+		elif restaurant_ratings.get(next) == None:
+			print "Still trying to get a reservation. Maybe try Urbun Burger?"
+			continue
+		else:
+			print "The rating for %s is %d" % (next, restaurant_ratings[restaurant])
+			continue
+
+rest_rate()
